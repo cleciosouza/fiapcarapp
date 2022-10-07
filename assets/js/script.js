483118ef7ea8e7ff00a1f49e2420ee67e6e4dd14
Clécio Souza <cleciodesign@gmail.com>
2022-10-06 22:04:55 -0300

    Versão 1

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+function gate() {
+    var element = document.getElementById("iconGate");    
+    element.classList.toggle("iconGateClose");
+    var textGate = document.getElementById("textGate");
+    textGate.classList.toggle("closed");
+  }
+
+//   function myFunction() {
+//     var element = document.getElementById("myDIV");
+//     element.classList.toggle("mystyle");
+//  }
